2da2b5657424550712055224288d5b1918e49e0e
sandunika123 <amdilshanisandunika@gmail.com>
2023-05-12 15:36:03 +0530

    first commit

---
+++ includes/header.php
@@ -0,0 +1,110 @@
+<?php if($_SESSION['login'])
+{?>
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
+			<li class="prnt"><a href="profile.php">My Profile</a></li>
+				<li class="prnt"><a href="change-password.php">Change Password</a></li>
+			<li class="prnt"><a href="tour-history.php">My Tour History</a></li>
+			<li class="prnt"><a href="issuetickets.php">Issue Tickets</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol">Welcome :</li>				
+			<li class="sig"><?php echo htmlentities($_SESSION['login']);?></li> 
+			<li class="sigi"><a href="logout.php" >/ Logout</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div><?php } else {?>
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.php"><i class="fa fa-home"></i></a></li>
+				<li class="hm"><a href="admin/index.php">Admin Login</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+</svg></li>		
+<li class="tol"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+</svg></li>		
+<li class="tol">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-messenger" viewBox="0 0 16 16">
+  <path d="M0 7.76C0 3.301 3.493 0 8 0s8 3.301 8 7.76-3.493 7.76-8 7.76c-.81 0-1.586-.107-2.316-.307a.639.639 0 0 0-.427.03l-1.588.702a.64.64 0 0 1-.898-.566l-.044-1.423a.639.639 0 0 0-.215-.456C.956 12.108 0 10.092 0 7.76zm5.546-1.459-2.35 3.728c-.225.358.214.761.551.506l2.525-1.916a.48.48 0 0 1 .578-.002l1.869 1.402a1.2 1.2 0 0 0 1.735-.32l2.35-3.728c.226-.358-.214-.761-.551-.506L9.728 7.381a.48.48 0 0 1-.578.002L7.281 5.98a1.2 1.2 0 0 0-1.735.32z"/>
+</svg> &ensp;&ensp;&ensp;&ensp;
+</li>	
+	
+			<li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
+			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Login</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<?php }?>
+<!--- /top-header ---->
+<!--- footer-btm ---->
+<div class="footer-btm wow fadeInLeft animated" data-wow-delay=".5s"style="background-color: rgb(244, 247, 105); height: 90px;" >
+	
+	<div class="navigation" >
+			<nav class="navbar navbar-default" >
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>	
+					
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				
+    
+				<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: rgb(244, 247, 105);">
+  			<div class="container-fluid">
+    			<a class="navbar-brand" href="index.php"><img src="images/logo.png" width="107" height="60"></a></a>
+    			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      				<span class="navbar-toggler-icon"></span>
+    			</button>
+   			 <div class="collapse navbar-collapse" id="navbarNav">
+      			<ul class="navbar-nav">
+       			 <li class="nav-item">
+          			<a class="nav-link active" aria-current="page" href="index.php">Home</a>
+       			 </li>
+				<li class="nav-item">
+				<a class="nav-link" href="page.php?type=aboutus">About</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="package-list.php">Tour Packages</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="page.php?type=privacy">Privacy Policy</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="page.php?type=terms">Terms of Use</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="page.php?type=contact">Contact Us</a>
+				</li>
+								
+      			</ul>
+    		</div>
+  			</div>
+			</nav>
+
+				
+						
+							
+								
+
+						
+					</nav>
+				</div><!-- /.navbar-collapse -->	
+			</nav>
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
